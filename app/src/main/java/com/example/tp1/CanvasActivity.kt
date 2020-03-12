e3378cafdb9adcc2a768eabd4cf1bd0a497c8fcb
Grégory Alary <gregalary26@gmail.com>
2020-03-12 12:45:38 +0100

    shake filter

--- app/src/main/java/com/example/tp1/CanvasActivity.kt
+++ app/src/main/java/com/example/tp1/CanvasActivity.kt
@@ -1,10 +1,17 @@
 package com.example.tp1
 
+import android.R.attr.src
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.graphics.*
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.MotionEvent
 import android.view.View
@@ -22,10 +29,15 @@ import java.io.File
 import java.io.InputStream
 
 
-class CanvasActivity : Activity() {
+class CanvasActivity : Activity(), SensorEventListener {
 
+    private var last_x: Float = 0f
+    private var last_y: Float = 0f
+    private var last_z: Float = 0f
+    private var lastUpdate: Long = 0
     private lateinit var btnChooseImage: ImageView;
     private lateinit var btnChooseColor: ImageView;
+    private lateinit var btnNextFilter: ImageView;
     private lateinit var canvas: Canvas;
     private var paint: Paint = Paint();
     private lateinit var bitmap: Bitmap;
@@ -33,10 +45,19 @@ class CanvasActivity : Activity() {
     private var firstTouch: Boolean = true;
     private var imagePath: String? = null;
     private var originalImage: Bitmap? = null;
+    private var originalFilterImage: Bitmap? = null;
+    private var busy: Boolean = false;
 
     private var oldX: Float = -1f;
     private var oldY: Float = -1f;
     private lateinit  var s: Sticker;
+
+    enum class FilterType {
+        NORMAL,
+        BLACK_AND_WHITE,
+        INVERTED
+    }
+    private var currentFilter: FilterType = FilterType.NORMAL
 
     private lateinit var powerMenu: PowerMenu;
 
@@ -56,6 +77,7 @@ class CanvasActivity : Activity() {
         paint.strokeWidth = 10f
         btnChooseImage = findViewById(R.id.btnLoad)
         btnChooseColor = findViewById(R.id.btnColor)
+        btnNextFilter = findViewById(R.id.btnNextFilter)
         println("================= > On passe dans mon code")
         imageView.setOnTouchListener(object : View.OnTouchListener {
             override fun onTouch(v: View, event: MotionEvent?): Boolean {
@@ -88,7 +110,8 @@ class CanvasActivity : Activity() {
         })
         btnChooseImage.setOnClickListener(View.OnClickListener { chooseImage() })
         btnChooseColor.setOnClickListener(View.OnClickListener { chooseColor() })
-        imagePath = intent.getStringExtra("imagePath");
+        btnNextFilter.setOnClickListener(View.OnClickListener { nextFilter() })
+        s = Sticker(R.drawable.emoji_happy, 0f, 0f, 256f, 256f)
         powerMenu = PowerMenu.Builder(this)
             .addItemList(listOf(PowerMenuItem("Novel"), PowerMenuItem("Poetry"), PowerMenuItem("Art"))) // list has "Novel", "Poerty", "Art"
             .addItem(PowerMenuItem("Journals", false)) // add an item.
@@ -104,7 +127,14 @@ class CanvasActivity : Activity() {
             .setSelectedMenuColor(ContextCompat.getColor(this, R.color.colorPrimary))
             .setOnMenuItemClickListener(onMenuItemClickListener)
             .build()
-        loadPicture(Uri.fromFile(File(imagePath)))
+        try {
+            imagePath = intent.getStringExtra("imagePath");
+            loadPicture(Uri.fromFile(File(imagePath)))
+        } catch (e: Exception) {
+            // hack
+        }
+        var mSensorMgr = this.getSystemService(Context.SENSOR_SERVICE) as SensorManager;
+        mSensorMgr.registerListener(this, mSensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     private fun chooseColor() {
@@ -138,11 +168,11 @@ class CanvasActivity : Activity() {
     private fun loadPicture(exifData: Uri) {
         val ins: InputStream? = getContentResolver()?.openInputStream(exifData);
         originalImage = BitmapFactory.decodeStream(ins);
+        originalFilterImage = originalImage!!.copy(Bitmap.Config.ARGB_8888, true);
         bitmap = originalImage!!.copy(Bitmap.Config.ARGB_8888, true);
-
         if (bitmap != null) {
             redrawImageView()
-            applyBlackAndWhiteFilter()
+            drawSticker(s)
         } else {
             System.out.println("IMG IS NULL")
         }
@@ -182,16 +212,9 @@ class CanvasActivity : Activity() {
     }
 
     private fun drawSticker(st: Sticker) {
-        // redraw scene
-
-        if (originalImage != null) {
-            bitmap = originalImage!!.copy(Bitmap.Config.ARGB_8888, true);
-            imageView.setImageBitmap(bitmap!!);
-            canvas = Canvas(bitmap!!);
-            firstTouch = false;
-
-        }
-
+        bitmap = originalFilterImage!!.copy(Bitmap.Config.ARGB_8888, true);
+        imageView.setImageBitmap(bitmap!!);
+        canvas = Canvas(bitmap!!);
         // draw current sticker
         var paint0 = Paint();
         paint.isAntiAlias = false;
@@ -202,20 +225,107 @@ class CanvasActivity : Activity() {
         imageView.invalidate()
     }
 
+    private fun applyInvertedFilter() {
+        // image size
+        // image size
+        val width: Int = originalImage!!.getWidth()
+        val height: Int = originalImage!!.getHeight()
+        // create output bitmap
+        // create output bitmap
+        val bmOut = Bitmap.createBitmap(originalImage!!.width, originalImage!!.height, originalImage!!.getConfig())
+        // color information
+        // color information
+        var A: Int
+        var R: Int
+        var G: Int
+        var B: Int
+        var pixel: Int
+        // scan through all pixels
+        // scan through all pixels
+        for (x in 0 until width) {
+            for (y in 0 until height) { // get pixel color
+                pixel = originalImage!!.getPixel(x, y)
+                // get color on each channel
+                A = Color.alpha(pixel)
+                R = Color.red(pixel)
+                G = Color.green(pixel)
+                B = Color.blue(pixel)
+                // set new pixel color to output image
+                bmOut.setPixel(x, y, Color.argb(A, 255 - R, 255 - G, 255 - B))
+            }
+        }
+        originalFilterImage = bmOut.copy(Bitmap.Config.ARGB_8888, true)
+        drawSticker(s)
+    }
+
     private fun applyBlackAndWhiteFilter() {
-        //         imageView.setColorFilter(Color.RED, PorterDuff.Mode.LIGHTEN);
-        val mxA = ColorMatrix()
-        mxA.setSaturation(0f);
-        val mxB = ColorMatrix();
-        mxB.setScale(1f, .95f, .82f, 1.0f);
-        mxA.setConcat(mxB, mxA);
-        val paint = Paint();
-        paint.setColorFilter(ColorMatrixColorFilter(mxA));
-        canvas.drawBitmap(bitmap, 0f, 0f, paint)
+        val bwBitmap = Bitmap.createBitmap(originalImage!!.width, originalImage!!.height, Bitmap.Config.ARGB_8888)
+        val hsv = FloatArray(3)
+        for (col in 0 until originalImage!!.width) {
+            for (row in 0 until originalImage!!.height) {
+                Color.colorToHSV(originalImage!!.getPixel(col, row), hsv)
+                if (hsv[2] > 0.5f) {
+                    bwBitmap.setPixel(col, row, -0x1)
+                } else {
+                    bwBitmap.setPixel(col, row, -0x1000000)
+                }
+            }
+        }
+        originalFilterImage = bwBitmap.copy(Bitmap.Config.ARGB_8888, true)
+        drawSticker(s)
+    }
 
-        s = Sticker(R.drawable.emoji_happy, 0f, 0f, 256f, 256f);
-        drawSticker(s);
+    private fun applyNormalFilter() {
+        // todo
+        originalFilterImage = originalImage!!.copy(Bitmap.Config.ARGB_8888, true);
+        drawSticker(s)
+    }
 
+    private fun nextFilter() {
+        // busy = true
+        when (currentFilter) {
+            FilterType.NORMAL -> {
+                applyBlackAndWhiteFilter()
+                currentFilter = FilterType.BLACK_AND_WHITE
+            }
+            FilterType.BLACK_AND_WHITE -> {
+                applyInvertedFilter()
+                currentFilter = FilterType.INVERTED
+            }
+            FilterType.INVERTED -> {
+                applyNormalFilter()
+                currentFilter = FilterType.NORMAL
+            }
+        }
+        // busy = false
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // nothing
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (!busy) {
+            busy = true
+            val curTime = System.currentTimeMillis()
+            // only allow one update every 100ms.
+            // println(curTime)
+            if (curTime - lastUpdate > 100) {
+                val diffTime = curTime - lastUpdate
+                lastUpdate = curTime
+                val x = event!!.values[SensorManager.DATA_X]
+                val y = event!!.values[SensorManager.DATA_Y]
+                val z = event!!.values[SensorManager.DATA_Z]
+                val speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000
+                if (speed > 7000) {
+                    nextFilter()
+                }
+                last_x = x
+                last_y = y
+                last_z = z
+            }
+            busy = false
+        }
     }
 
 }
